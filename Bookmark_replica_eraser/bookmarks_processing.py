6a65fdd947a07f447d4456ec4e9fc0ccaf619a2e
Przemysław S <przemyslaw.siekaniec@gmail.com>
2025-07-27 22:52:39 +0200

    Add required strs for JSON reading by web browser

--- Bookmark_replica_eraser/bookmarks_processing.py
+++ Bookmark_replica_eraser/bookmarks_processing.py
@@ -7,15 +7,15 @@ def main(filename):
     target_path = os.path.join(os.getcwd(), filename)
     content = read_content(target_path)
 
-    save_pretty_json("pretty1.json", content)
-    content = read_content("pretty1.json")
+    save_pretty_json("pretty2.json", content)
+    content = read_content("pretty2.json")
 
     folders = get_folders_of_bookmarks(content)
     result = get_unique_bookmarks(folders)
 
     print(f"Found: {len(result)} unique bookmark(s)")
 
-    save_pretty_json("pretty.json", result)
+    save_pretty_json("pretty_final.json", result)
 
 
 def get_unique_bookmarks(folders_of_bookmarks, key="uri"):
@@ -42,7 +42,73 @@ def get_folders_of_bookmarks(content):
     ]
 
 
-def save_pretty_json(path, content):
+def save_pretty_json(path, content, final=False):
+    if final:
+        content = (
+            """
+        { "children": 
+            [
+                {
+                    "dateAdded": 1753605265591000,
+                    "guid": "menu________",
+                    "id": 2,
+                    "index": 0,
+                    "lastModified": 1753647823370000,
+                    "root": "bookmarksMenuFolder",
+                    "title": "menu",
+                    "type": "text/x-moz-place-container",
+                    "typeCode": 2
+                },
+                {
+                "children": """
+            + content
+            + """
+                ,
+                        "dateAdded": 1753605265591000,
+                        "guid": "toolbar_____",
+                        "id": 3,
+                        "index": 1,
+                        "lastModified": 1753648620860000,
+                        "root": "toolbarFolder",
+                        "title": "toolbar",
+                        "type": "text/x-moz-place-container",
+                        "typeCode": 2
+                    },
+                    {
+                        "dateAdded": 1753605265591000,
+                        "guid": "unfiled_____",
+                        "id": 5,
+                        "index": 3,
+                        "lastModified": 1753647823370000,
+                        "root": "unfiledBookmarksFolder",
+                        "title": "unfiled",
+                        "type": "text/x-moz-place-container",
+                        "typeCode": 2
+                    },
+                    {
+                        "dateAdded": 1753605265603000,
+                        "guid": "mobile______",
+                        "id": 6,
+                        "index": 4,
+                        "lastModified": 1753647823370000,
+                        "root": "mobileFolder",
+                        "title": "mobile",
+                        "type": "text/x-moz-place-container",
+                        "typeCode": 2
+                    }
+            ],
+            "dateAdded": 1753605265591000,
+            "guid": "root________",
+            "id": 1,
+            "index": 0,
+            "lastModified": 1753648620860000,
+            "root": "placesRoot",
+            "title": "",
+            "type": "text/x-moz-place-container",
+            "typeCode": 2
+        }"""
+        )
+
     with open(path, "w") as pretty:
         json.dump(content, pretty, indent=4, sort_keys=True)
 
