c2c78c1f2e3bc4fea22ae0a41b6a17f826665b0b
Przemysław S <przemyslaw.siekaniec@gmail.com>
2025-07-27 21:15:40 +0200

    Black reformat

--- Bookmark_replica_eraser/bookmarks_processing.py
+++ Bookmark_replica_eraser/bookmarks_processing.py
@@ -1,8 +1,9 @@
 import json
 import os
 
+
 def main(filename):
-    content = ''
+    content = ""
     target_path = os.path.join(os.getcwd(), filename)
     with open(target_path, "r") as file:
         content = json.load(file)
@@ -13,22 +14,28 @@ def main(filename):
     with open("pretty.json", "r") as pretty:
         content = json.load(pretty)
 
-    lists_of_bookmarks = [dict_with_key for dict_with_key in content["children"] if "children" in dict_with_key]
+    lists_of_bookmarks = [
+        dict_with_key
+        for dict_with_key in content["children"]
+        if "children" in dict_with_key
+    ]
     result = []
     for l in lists_of_bookmarks:
         try:
             bookmarks = sorted(l["children"], key=lambda b: b["uri"])
             for i in range(len(bookmarks)):
-                if not bookmarks[i-1]["uri"] == bookmarks[i]["uri"] or len(bookmarks) == 1:
+                if (
+                    not bookmarks[i - 1]["uri"] == bookmarks[i]["uri"]
+                    or len(bookmarks) == 1
+                ):
                     result.append(bookmarks[i])
-                        
+
         except KeyError:
             print(f"Invalid list of bookmarks")
-    
+
     print(result)
 
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     filename = "bookmarks-2025-07-27.json"
     main(filename)
