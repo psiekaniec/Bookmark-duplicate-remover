4af2d77450f72e9a0757699bbf16a73223d66470
Przemysław S <przemyslaw.siekaniec@gmail.com>
2025-07-28 09:35:13 +0200

    Replacing big JSON string with a dict of folders

--- Bookmark_replica_eraser/bookmarks_processing.py
+++ Bookmark_replica_eraser/bookmarks_processing.py
@@ -2,20 +2,20 @@ import json
 import os
 
 
-def main(filename):
+def main(filename, output_filename="pretty.json"):
     content = ""
     target_path = os.path.join(os.getcwd(), filename)
     content = read_content(target_path)
 
-    save_pretty_json("pretty2.json", content)
-    content = read_content("pretty2.json")
+    save_pretty_json(output_filename, content)
+    content = read_content(output_filename)
 
     folders = get_folders_of_bookmarks(content)
     result = get_unique_bookmarks(folders)
 
     print(f"Found: {len(result)} unique bookmark(s)")
 
-    save_pretty_json("pretty_final.json", result)
+    save_pretty_json(output_filename, result)
 
 
 def get_unique_bookmarks(folders_of_bookmarks, key="uri"):
@@ -44,70 +44,13 @@ def get_folders_of_bookmarks(content):
 
 def save_pretty_json(path, content, final=False):
     if final:
-        content = (
-            """
-        { "children": 
-            [
-                {
-                    "dateAdded": 1753605265591000,
-                    "guid": "menu________",
-                    "id": 2,
-                    "index": 0,
-                    "lastModified": 1753647823370000,
-                    "root": "bookmarksMenuFolder",
-                    "title": "menu",
-                    "type": "text/x-moz-place-container",
-                    "typeCode": 2
-                },
-                {
-                "children": """
-            + content
-            + """
-                ,
-                        "dateAdded": 1753605265591000,
-                        "guid": "toolbar_____",
-                        "id": 3,
-                        "index": 1,
-                        "lastModified": 1753648620860000,
-                        "root": "toolbarFolder",
-                        "title": "toolbar",
-                        "type": "text/x-moz-place-container",
-                        "typeCode": 2
-                    },
-                    {
-                        "dateAdded": 1753605265591000,
-                        "guid": "unfiled_____",
-                        "id": 5,
-                        "index": 3,
-                        "lastModified": 1753647823370000,
-                        "root": "unfiledBookmarksFolder",
-                        "title": "unfiled",
-                        "type": "text/x-moz-place-container",
-                        "typeCode": 2
-                    },
-                    {
-                        "dateAdded": 1753605265603000,
-                        "guid": "mobile______",
-                        "id": 6,
-                        "index": 4,
-                        "lastModified": 1753647823370000,
-                        "root": "mobileFolder",
-                        "title": "mobile",
-                        "type": "text/x-moz-place-container",
-                        "typeCode": 2
-                    }
-            ],
-            "dateAdded": 1753605265591000,
-            "guid": "root________",
-            "id": 1,
-            "index": 0,
-            "lastModified": 1753648620860000,
-            "root": "placesRoot",
-            "title": "",
-            "type": "text/x-moz-place-container",
-            "typeCode": 2
-        }"""
-        )
+        content = {
+                "guid": "root________","id": 1,"index": 0,"root": "placesRoot","title": "","type": "text/x-moz-place-container","typeCode": 2,
+                "children": [{"guid": "menu________","id": 2,"index": 0,"root": "bookmarksMenuFolder","title": "menu","type": "text/x-moz-place-container","typeCode": 2},
+                             {"children": content,"guid": "toolbar_____","id": 3,"index": 1,"root": "toolbarFolder","title": "toolbar","type": "text/x-moz-place-container","typeCode": 2},
+                            {"guid": "unfiled_____","id": 5,"index": 3,"root": "unfiledBookmarksFolder","title": "unfiled","type": "text/x-moz-place-container","typeCode": 2},
+                    {"guid": "mobile______","id": 6,"index": 4,"root": "mobileFolder","title": "mobile","type": "text/x-moz-place-container","typeCode": 2}],
+        }
 
     with open(path, "w") as pretty:
         json.dump(content, pretty, indent=4, sort_keys=True)
